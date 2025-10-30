5623313483e6c62a1264c724a70f25b9075933eb
VyFaHtE <Chrshes@yandex.ru>
2025-10-31 00:55:32 +0300

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,17 @@
+def entr(s):
+    answ = {}
+    for i in s:
+        if i in answ: answ[i] += 1
+        else: answ[i] = 1
+    return answ
 
-
-if __name__ == "__main__":
-    pass # Ваш код здесь
+a, b = map(entr, input().split())
+if len(a) != len(b):
+    print('Количество элементов не равно. Установить все соотношеня невозможно')
+for i in a:
+    if not a[i] in list(b.values()):
+        print(i, '- невозможно найти соответствие', end=' ')
+    else:
+        for itm, val in b.items():
+            if val == a[i]:
+                print(f'{i}={itm}', end=' ')
