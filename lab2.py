e1fb7f207366343b51c2f74a4a348110084b0e16
vadim1-art <vadimgotovec1@gmail.com>
2025-11-13 20:43:59 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -2,20 +2,19 @@ from collections import deque
 
 
 def check_brackets(s):
-    # Словарь соответствия открывающих и закрывающих скобок
+
     bracket_pairs = {')': '(', '}': '{', ']': '['}
 
     stack = deque()
 
     for i, char in enumerate(s):
         if char in bracket_pairs.values():
-            stack.append((char, i))  # Добавляем её вместе с позицией
+            stack.append((char, i))
 
-        elif char in bracket_pairs.keys():  # Это закрывающая скобка?
+        elif char in bracket_pairs.keys():
             if not stack or stack[-1][0] != bracket_pairs[char]:
-                return f'Ошибка в позиции {i + 1}'  # Скобки не совпадают!
+                return f'Ошибка в позиции {i + 1}'
 
-            # Совпадение найдено, убираем пару из стека
             stack.pop()
 
     if stack:
@@ -27,3 +26,4 @@ def check_brackets(s):
 
 s = str(input())
 print(check_brackets(s))
+
